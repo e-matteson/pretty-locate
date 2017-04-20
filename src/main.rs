67518fe1f1bb7808d9dc74d8d71ec93306986504
e-matteson <e.r.matteson@gmail.com>
2017-04-19 23:54:40 -0400

    add readme

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,6 @@ extern crate pretty_locate;
 
 use pretty_locate::*;
 
-// TODO
-// - Make it a complete wrapper. Add support for all locate options, multiple patterns, etc.
-// - Optimize for speed: into_map(), get_paths()?
-// - Organize modules better?
-// - Do proper error-handling with Result
-// - Handle wildcard in formatting
-
 
 fn main() {
     let args = get_args();
